697a4eb9357727a491af0eaa2c620edc70cc73ad
senortrashcan <hnl9324@gmail.com>
2024-08-22 20:38:50 -0500

    Update hero.test.jsx
    testing

--- tests/unit/sections/hero.test.jsx
+++ tests/unit/sections/hero.test.jsx
@@ -8,7 +8,7 @@ describe('Hero section parts', () => {
 		render(<Hero />)
 		
 		const heading = screen.getByRole('heading', {
-			name: /Andrew Nelson\./i,
+			name: /Big coc\./i,
 		})
 		
 		expect(heading).toBeInTheDocument()
@@ -18,7 +18,7 @@ describe('Hero section parts', () => {
 		render(<Hero />)
 		
 		const heading = screen.getByRole('heading', {
-			name: /I design and build meaningful experiences\./i,
+			name: /Suck my balls\./i,
 		})
 		
 		expect(heading).toBeInTheDocument()
