d3978abc43b24953d22f61e39d72727c1424ff9e
senortrashcan <hnl9324@gmail.com>
2024-08-30 20:52:10 -0500

    testing

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,7 +3,7 @@ import Looking 		from '../components/sections/index/looking'
 import About 		from '../components/sections/index/about'
 import Technical 	from '../components/sections/index/technical'
 import Career 		from '../components/sections/index/career'
-import FeaturedProjects	from '../components/sections/projects/featured'
+/*import FeaturedProjects	from '../components/sections/projects/featured'*/
 
 import Color 		from '../components/utils/page.colors.util'
 
@@ -17,6 +17,7 @@ export default function HomePage() {
 			<Color colors={colors} />
 			<Hero />
 			{/* <Looking /> */}
+			<FeaturedProjects />
 			<About />
 			<Technical />
 			{/* <Career /> */}
