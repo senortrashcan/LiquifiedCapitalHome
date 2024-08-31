bbd26c6d70c3f46a045e600d24a5a5c9423f78ef
senortrashcan <hnl9324@gmail.com>
2024-08-30 21:23:18 -0500

    mebe?

--- components/utils/theme.util.jsx
+++ components/utils/theme.util.jsx
@@ -20,7 +20,7 @@ export default function SetTheme() {
 
 	const defaultTheme = () => {
 		const themeLocalStorage = localStorage.getItem('theme')
-		const themeSystem       = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+		const themeSystem       = window.matchMedia('(prefers-color-scheme: dark)')
 
 		return ( themeLocalStorage ?? themeSystem )
 	}
