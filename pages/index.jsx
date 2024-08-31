768ef4f0d54eabb40dbd2eed29deb04839f2130d
senortrashcan <hnl9324@gmail.com>
2024-08-30 20:54:51 -0500

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,7 +17,6 @@ export default function HomePage() {
 			<Color colors={colors} />
 			<Hero />
 			{/* <Looking /> */}
-			<FeaturedProjects />
 			<About />
 			<Technical />
 			{/* <Career /> */}
