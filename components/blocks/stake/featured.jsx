afc94e0223e13fb564201a90db62f1cd68a38ec6
senortrashcan <hnl9324@gmail.com>
2024-09-05 23:08:12 -0500

    plz

--- components/blocks/stake/featured.jsx
+++ components/blocks/stake/featured.jsx
@@ -48,7 +48,7 @@ export default function FeaturedProject({ content }, index) {
 						<Badges list={stack} block="stack" fullContainer={false} color={false} />
 					</div>
 					<m.div variants={''} className={css.viewProject}>
-						<Link to='www.minecraft.com'>
+						<Link to={'www.minecraft.com'}>
 							<Icon icon={[ 'fad', 'arrow-right-to-bracket' ]} />
 						</Link>
 					</m.div>
