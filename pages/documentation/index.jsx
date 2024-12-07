d2aede67caa07bf1d530a8309132c153e7d7a4c5
senortrashcan <hnl9324@gmail.com>
2024-12-07 02:50:27 -0600

    fix path

--- pages/documentation/index.jsx
+++ pages/documentation/index.jsx
@@ -2,7 +2,7 @@ import Color 	from '../../components/utils/page.colors.util'
 import ComingSoon from '../../components/sections/comingsoon'
 import DocumentationPage from '../../components/sections/docs/DocumentationPage'
 
-import colors 		from '../../content/case-studies/_colors.json'
+import colors 		from '../../content/documentation/_colors.json'
 import settings 	from '../../content/_settings.json'
 
 //
